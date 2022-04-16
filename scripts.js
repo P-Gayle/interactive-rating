ded3323d293de40fea214bfd3e4eb62e73d711a3
P-Gayle <103328828+P-Gayle@users.noreply.github.com>
2022-04-16 15:05:15 +0100

    Complete
    Finished the CSS for rating state and all js innerHTML

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
-const submit = document.querySelector(".submit");
 const card1 = document.querySelector (".card");
 const card2 = document.querySelector("#after-submit");
- const btn = document.querySelectorAll(".btn")
-
 
  function thanks(){
     document.getElementById("after-submit").style.visibility = "visible";
@@ -11,3 +8,32 @@ const card2 = document.querySelector("#after-submit");
     card2.style.display = "block";
 };
 
+document.getElementById("b1").addEventListener("click", showb1);
+
+function showb1() {
+    document.getElementById("rating1").innerHTML = "You selected 1 out of 5"              
+};
+
+document.getElementById("b2").addEventListener("click", showb2);
+
+function showb2() {
+    document.getElementById("rating2").innerHTML = "You selected 2 out of 5"              
+};
+
+document.getElementById("b3").addEventListener("click", showb3);
+
+function showb3() {
+    document.getElementById("rating3").innerHTML = "You selected 3 out of 5"              
+};
+
+document.getElementById("b4").addEventListener("click", showb4);
+
+function showb4() {
+    document.getElementById("rating4").innerHTML = "You selected 4 out of 5"              
+};
+
+document.getElementById("b5").addEventListener("click", showb5);
+
+function showb5() {
+    document.getElementById("rating5").innerHTML = "You selected 5 out of 5"              
+};
